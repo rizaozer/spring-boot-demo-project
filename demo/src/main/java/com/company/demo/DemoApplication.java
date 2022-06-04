599d3a6bd059e12a9e7c6b7794276408df56321d
rizaozer <rizaoozer@gmail.com>
2022-06-04 18:35:22 +0300

    create Employee.java

--- demo/src/main/java/com/company/demo/DemoApplication.java
+++ demo/src/main/java/com/company/demo/DemoApplication.java
@@ -1,13 +1,28 @@
 package com.company.demo;
 
+import com.company.demo.employee.Employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping
+	public List<Employee> hello() {
+		return List.of(new Employee(
+				1L,
+				"Riza Ozer",
+				"rizaoozer@gmail.com",
+				1200));
 	}
 
 }
---
+++ demo/src/main/java/com/company/demo/employee/Employee.java
@@ -0,0 +1,67 @@
+package com.company.demo.employee;
+
+
+public class Employee {
+    private Long id;
+    private String name;
+    private String email;
+    private double salary;
+
+    public Employee() {
+    }
+
+    public Employee(Long id, String name, String email, double salary) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.salary = salary;
+    }
+
+    public Employee(String name, String email, double salary) {
+        this.name = name;
+        this.email = email;
+        this.salary = salary;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
